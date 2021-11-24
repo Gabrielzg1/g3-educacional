a661b2572fde0124d241b2a9bffdad141df781e0
ripercario <88801928+ripercario@users.noreply.github.com>
2021-11-24 14:11:53 -0300

    Update main.js

--- pages/game/main.js
+++ pages/game/main.js
@@ -7,7 +7,7 @@ var player = {
     lado: 100,
     color: "blue",
     x: 100,
-    y: 100,
+    y: 750,
     velocidade: 10,
 }
 
@@ -28,21 +28,12 @@ document.addEventListener('keydown', function (e) {
     if (e.keyCode == 37) {
         dx = -1;
     }
-    //top
-    else if (e.keyCode == 38) {
-
-        dy = -1;
-    }
     //right
     else if (e.keyCode == 39) {
 
         dx = 1;
     }
-    //bottom
-    else if (e.keyCode == 40) {
 
-        dy = 1;
-    }
 });
 document.addEventListener('keyup', function (e) {
     switch (e.keyCode) {
@@ -52,11 +43,6 @@ document.addEventListener('keyup', function (e) {
             dx = 0;
             break;
 
-        case 38: // up
-        case 40: // down
-
-            dy = 0;
-            break;
     }
 
 
@@ -67,23 +53,14 @@ function Desenhar() {
         player.x -= player.velocidade
     if (dx == 1)
         player.x += player.velocidade
-    if (dy == 1)
-        player.y += player.velocidade
-    if (dy == -1)
-        player.y -= player.velocidade
 
 
     if (player.x > 1890 - player.lado) {
         player.x = 1890 - player.lado
     }
-    if (player.y > 930 - player.lado)
-        player.y = 930 - player.lado
 
     if (player.x < 0)
         player.x = 0
-
-    if (player.y < 0)
-        player.y = 0
 
 
     ctx.fillRect(player.x, player.y, player.lado, player.lado)

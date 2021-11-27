bf587d5f8e72eb817e3ddaaf721e698ff7c65fbe
Gabrielzg1 <gabrielzg173@gmail.com>
2021-11-27 14:57:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
-var express = require('express');
+const express = require('express');
+const session = require('express-session')
 var app = express();
+
 const port = 3000
 const host = 'localhost'
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("pages"));
 
 app.get('/home', function (req, res) {
 
 });
 
-//configurando server da página de login
-app.get('/login', function (req, res) {
-    var nome = req.query.nome;
+app.use(session({
+    secret: "asfdasdfasfsadfasdfasdfadfs",
+    secure: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365
+    },
+    saveUninitialized: false
+}));
 
+//configurando server da página de login
+app.post('/login', function (req, res) {
+    if (req.session.userid || req.body.passwd == "123456") {
+        req.session.userid = req.body.userid;
+        CarregarEmails(req, res);
+    } else {
+        res.send("Senha Inválida");
+    }
 });
-app.get('/getNome', function (req, res) {
-    res.send(Nome);
+
+app.get('/login', function (req, res) {
+    if (req.session.userid) {
+        CarregarEmails(req, res);
+    } else {
+        res.send("Usuário não está logado!");
+    }
 });
 
 

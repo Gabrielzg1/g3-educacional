3bd0bfc93e9e6e8cc09ff04b2bc14ee4160d69c2
Gabrielzg1 <gabrielzg173@gmail.com>
2021-12-01 15:05:28 -0300

    atualização server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const session = require('express-session')
 var app = express();
+const path = require('path');
 
 const port = 3030
 const host = 'localhost'
 
-/*app.use(express.urlencoded({ extended: false }));
-app.use(express.json());*/
-app.use(express.static("pages"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("www"));
 
-/*app.use(session({
+
+app.use(session({
     secret: "asfdasdfasfsadfasdfasdfadfs",
     secure: false,
     resave: false,
@@ -17,26 +19,38 @@ app.use(express.static("pages"));
         maxAge: 0.5 * 60 * 60 * 24 * 365
     },
     saveUninitialized: false
-}));*/
+}));
 
 //configurando server da página de login
 app.get('/login', function (req, res) {
-    /*var username = false
+    res.sendFile(path.join(__dirname, './login/index.html'));
     if (req.session.userid) {
-        req.session.userid = req.query.userid;
-    } else {
-        username = req.query.userid
-        console.log(username)
+        res.redirect('/game')
+    }
 
-    }*/
 });
 
-app.get('/home', function (req, res) {
-
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './home/index.html'));
 });
 //configuração do jogo
 app.get('/game', function (req, res) {
+    res.sendFile(path.join(__dirname, './game/index.html'));
+    var teste = "entrou"
+    if (req.session.userid) {
+        console.log(teste)
+        res.sendFile(__dirname, './game/index.html')
 
+    } else {
+        if (req.query.userid) {
+            req.session.userid = req.query.userid
+            console.log(teste + '1')
+            res.sendFile(__dirname, './game/index.html')
+        } else {
+            console.log(teste + '2');
+            res.redirect('/login')
+        }
+    }
 
 });
 app.get('/logout', function (req, res) {
@@ -46,6 +60,6 @@ app.get('/logout', function (req, res) {
 });
 
 app.listen(port, function () {
-    console.log(`Rodando em: http://${host}:${port}/home`);
+    console.log(`Rodando em: http://${host}:${port}/`);
 });
 
